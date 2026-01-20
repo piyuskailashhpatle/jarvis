b8fec48d0d76ebd0adde29c7477d6b68987e9c74
Karan Deshmukh <karanjagandeshmukh@gmail.com>
2026-01-20 15:06:24 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,33 @@
+import pyttsx3
+from datetime import datetime
+import requests
+import json
+
+def speak(text):
+    engine = pyttsx3.init('sapi5')
+    engine.setProperty('rate', 170)
+    engine.setProperty('volume', 1.0)
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
+print("🚀 Jarvis activited sucessfully!")
+speak("Hello Karan")
+
+
+while True:
+    command = input("Type command: ").lower().strip()
+
+    if command == "exit":
+        speak("Goodbye Karan")
+        break
+
+    elif "hello" in command:
+        speak("Hello Karan, how can I help you?")
+
+    elif "time" in command:
+        time = datetime.now().strftime("%H:%M")
+        speak(f"The time is {time}")
+
+    else:
+        speak("Sorry, I did not understand")
